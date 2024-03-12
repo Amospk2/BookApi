bd963f51b1776460724f9a99db71810af92fb418
Amospk2 <amospk2@Amospk2.Amospk2>
2024-03-12 12:56:18 -0300

    feat: application middlewares

---
+++ infra/middleware/auth.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type UserClaims struct {
+	User string `json:"user"`
+	Exp  string `json:"exp"`
+}
+
+func AuthenticationMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		header := strings.Split(r.Header.Get("Authorization"), " ")
+
+		if len(header) == 0 || !strings.Contains(header[0], "Bearer") {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		accessToken := header[1]
+		if accessToken == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		parsedAccessToken, _ := jwt.ParseWithClaims(
+			accessToken, jwt.MapClaims{},
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(os.Getenv("SECRET")), nil
+			},
+		)
+
+		if !parsedAccessToken.Valid {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", parsedAccessToken.Claims)
+
+		r = r.WithContext(ctx)
+
+		next(w, r)
+	})
+}
